800dac34d03b5856a319b3d5d9735c46af2a90b9
Juha Sonck <juha.sonck@pointcollege.fi>
2016-04-12 11:21:12 +0300

    Lisätty taulukko

---
+++ BootStrapTesti/BootStrapTesti/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BootStrapTesti.Startup))]
+namespace BootStrapTesti
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
